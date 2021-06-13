0e9cdce84d81bd72c173fab01d06560501afa13f
ashleypeake23 <ashleypeake23@gmail.com>
2021-06-13 17:25:21 -0400

    set up sensor initialization

--- envs/pybullet_sim.py
+++ envs/pybullet_sim.py
@@ -5,6 +5,8 @@ import yaml
 
 from wheeledRobots.clifford.cliffordRobot import Clifford
 from wheeledSim.simController import simController
+from wheeledSim.front_camera_sensor import FrontCameraSensor
+from wheeledSim.lidar_sensor import LidarSensor
 
 
 class WheeledSimEnv:
@@ -12,31 +14,45 @@ class WheeledSimEnv:
     Wrapper class to make a gym environment from pybullet sim
     """
 
-    def __init__(self, config_file, use_images=False, T=-1, render=True):
+    def __init__(self, config_file, T=-1, render=True):
 
+        # open file with configuration parameters
         stream = open(config_file, 'r')
         config = yaml.load(stream, Loader=yaml.FullLoader)
 
         self.client = pybullet.connect(pybullet.GUI) if render else pybullet.connect(pybullet.DIRECT)
         self.robot = Clifford(params=config['cliffordParams'], physicsClientId=self.client)
 
+        # initialize all sensors from config file
+        sensors = []
+        sense_dict = config['sensors']
+        for sensor in config['sensors']:
+            # TODO: Test (I can't import rospy so I can't currently test this to see if it works, but this is the idea)
+            sensors.append(sense_dict[sensor]['init'](self.robot, senseParamsIn=sense_dict[sensor]['params'],
+                                                      physicsClientId=self.client))
+
+        # load simulation environment
         self.env = simController(self.robot, self.client, config['simulationParams'], config['senseParams'],
                                  config['terrainMapParams'], config['terrainParams'], config['explorationParams'])
 
         self.T = T  # max steps allowed
         self.nsteps = 0  # number of steps taken
-        self.use_images = use_images
 
     @property
     def observation_space(self):
         # observation takes form (x,y,z) position, (x,y,z,w) quaternion orientation, velocity, joint state
         state_space = gym.spaces.Box(low=np.ones(13) * -float('inf'), high=np.ones(13) + float('inf'))
+
+        # TODO: Incorporate sensing data into state space?
+        """
         if not self.use_images:
             return state_space
         else:
             image_space = gym.spaces.Box(low=np.ones(self.env.senseParams['senseResolution']) * -float('inf'),
                                          high=np.ones(self.env.senseParams['senseResolution']) * float('inf'))
             return gym.spaces.Dict({'state': state_space, 'image': image_space})
+        """
+        return state_space
 
     @property
     def action_space(self):
@@ -46,30 +62,20 @@ class WheeledSimEnv:
     def reset(self):
         # reset environment
         self.env.newTerrain()
-        self.nsteps = 0
-        self.env.controlLoopStep([0., 0.])
         self.env.resetRobot()
+        self.nsteps = 0
 
         # get new observation
-        pose = self.env.robot.getPositionOrientation()
-        vel = self.robot.getBaseVelocity_body()
-        joints = self.robot.measureJoints()
-        if self.env.senseParams['recordJointStates']:
-            obs = list(pose[0]) + list(pose[1]) + vel[:] + joints[:]
-        else:
-            obs = list(pose[0]) + list(pose[1]) + vel[:]
+        obs = self.env.getObservation()
 
-        if self.use_images:
-            hmap = self.env.sensing(pose, senseType=0)
-            return {"state": np.array(obs), "image": hmap}
-
-        else:
-            return np.array(obs)
+        return np.array(obs)
 
     def step(self, action):
         # get state action, next state, and boolean terminal state from simulation
-        sa, s, sim_t = self.env.controlLoopStep(action)  # image is sa[1]
-        obs = {"state": np.array(s)[0], "image": sa[1]} if self.use_images else np.array(s)[0]
+        state_action, next_state, sim_t = self.env.controlLoopStep(action)  # image is sa[1]
+
+        # TODO: figure out incorporating sensing data to observation
+        obs = {"state": np.array(next_state)[0]}
 
         # increment number of steps and set terminal state if reached max steps
         self.nsteps += 1
@@ -89,7 +95,7 @@ class WheeledSimEnv:
 if __name__ == '__main__':
 
     """load environment"""
-    config_file = "configurations/cliffordExampleParams.yaml"
+    config_file = "../configurations/cliffordExampleParams.yaml"
     env = WheeledSimEnv(config_file, T=50)
 
     # run simulation 5 times
